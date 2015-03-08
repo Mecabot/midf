837fcd7999a955361b2897d37c0bdfb3cbe2480f
Saul Martinez Vidals <saul.martinez@linio.com.mx>
2015-03-08 13:54:03 -0600

    Icons for left links

--- midf/protected/views/layouts/column2.php
+++ midf/protected/views/layouts/column2.php
@@ -4,10 +4,9 @@
 <div class="col-md-3" role="complementary">
     <nav class="bs-docs-sidebar hidden-print hidden-xs hidden-sm">
         <ul class="nav bs-docs-sidenav">
-            <li><a href="<?= Yii::app()->getUrlManager()->createUrl('report') ?>">Nuevo reporte</a></li>
-            <li><a href="index.php?r=seguimiento/uno">Seguimiento</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Lo más reportado</a></li>
+            <li><a href="<?= Yii::app()->getUrlManager()->createUrl('report') ?>"><i class="fa fa-plus fa-left"></i>Nuevo reporte</a></li>
+            <li><a href="index.php?r=seguimiento/uno"><i class="fa fa-exchange fa-left"></i>Seguimiento</a></li>
+            <li><a href="#"><i class="fa fa-line-chart fa-left"></i>Lo más reportado</a></li>
         </ul>
     </nav>
 </div>

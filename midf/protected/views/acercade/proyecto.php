060cb66850ca5bce1bd495d59f396649a80df691
Daniel Palacios <daniel.palacios@linio.com>
2015-03-08 14:37:54 -0600

    add lo mas reportado

--- midf/protected/views/acercade/proyecto.php
+++ midf/protected/views/acercade/proyecto.php
@@ -20,7 +20,6 @@
 <ul>
     <li><a href="http://datos.labcd.mx/dataset/reportes-atendidos-en-el-2014/resource/388cc248-45b6-4736-895a-543044a45e2c">Reportes de Infraestructura atendidos 2014.</a> </li>
     <li><a href="http://datos.labcd.mx/dataset?_organization_limit=0&organization=instituto-de-las-mujeres">Información del programa Viajemos Seguras.</a> </li>
-    <li><a href="http://datos.labplc.mx/servicios/buzon.info?submodule">Programa jóvenes en desarrollo.</a> </li>
     <li><a href="http://datos.labcd.mx/dataset/intervenciones-en-la-via-publica">Intervenciones en la vía pública.</a> </li>
     <li><a href="http://datos.labcd.mx/dataset/reportes-atendidos-en-el-2014">Reportes atendidos en el 2014.</a> </li>
     <li><a href="http://datos.labcd.mx/dataset/solicitudes-de-abastos-de-agua">Solicitudes de abastos de agua.</a> </li>
@@ -37,69 +36,3 @@
     <li><a href="http://sc.inegi.org.mx/cobdem/contenido.jsp?rf=false&solicitud=">Sistema Estatal y Municipal de Bases de Datos (SIMBAD)</a> </li>
 </ul>
 
-<h2><b>Agencias Gubernamentales</b></h2>
-<ul>
-
-    <li><a href="">Coordinación de Control de Gestión Institucional de la Secretaría de Seguridad Pública del Distrito Federal</a>
-        <ul>
-            <li>Robo de Vehículo</li>
-            <li>Robo a Negocio</li>
-            <li>Robo a Casa Habitación</li>
-            <li>Robo a Repartidor</li>
-            <li>Robo de Autopartes</li>
-            <li>Venta de Droga</li>
-        </ul>
-    </li>
-    <p>-----------------------------------------------------------------</p>
-    <li><a href="">Consejo para Prevenir y Eliminar la  Discriminación</a>
-        <ul>
-            <li>Bullying</li>
-            <li>Mobbing</li>
-            <li>Discriminación LGBTTTI</li>
-            <li> Discriminación Discapacidad</li>
-            <li>Discriminación Pueblos Indígenas</li>
-            <li>Discriminación Migrantes</li>
-        </ul>
-    </li>
-    <p>-----------------------------------------------------------------</p>
-    <li><a href="">Sistema de Aguas de la Ciudad de México</a>
-        <ul>
-            <li>Fuga de Agua</li>
-            <li>Suministro de Agua</li>
-            <li>Desazolve</li>
-        </ul>
-    </li>
-    <p>-----------------------------------------------------------------</p>
-    <li><a href="">Centro de Servicios  y Atención Ciudadana</a>
-        <ul>
-            <li>Señalamientos</li>
-            <li>Alumbrado Público</li>
-        </ul>
-    </li>
-    <p>-----------------------------------------------------------------</p>
-    <li><a href="">Sistema de Atención Inmediata a Daños en la Infraestructura Urbana del Distrito Federal</a>
-        <ul>
-            <li>Áreas Verdes</li>
-            <li>Baches</li>
-        </ul>
-    </li>
-    <p>-----------------------------------------------------------------</p>
-    <li><a href="">Contraloría General del Distrito Federal</a>
-        <ul>
-            <li>Servidor Público</li>
-        </ul>
-    </li>
-
-    <p>-----------------------------------------------------------------</p>
-    <li><a href="">Sistema de Transporte Individual Ecobici</a>
-        <ul>
-            <li>Ecobici</li>
-        </ul>
-    </li>
-    <p>-----------------------------------------------------------------</p>
-    <li><a href="">Centro de Atención del Secretario</a>
-        <ul>
-            <li>Graffiti</li>
-        </ul>
-    </li>
-</ul>
--- midf/protected/views/acercade/quienes_somos.php
+++ midf/protected/views/acercade/quienes_somos.php
@@ -1,14 +1,10 @@
-<?php $this->setPageTitle('Quienes Somos'); ?>
+<?php $this->setPageTitle('¿Quiénes Somos?'); ?>
 <br>
-<p  align="center"><img src="images/ginyu_force.png"></p>
-
-
 <p>Somos un grupo de emprenderores que entiende la creación de contenidos de calidad como el fundamento del futuro de Internet.
     </p>
 
 
 <TABLE WIDTH=500>
-
     <TR>
         <TD WIDTH=100><img src="images/ara.png" WIDTH=158 HEIGHT=160>
         </TD>
@@ -18,8 +14,6 @@
         <TD WIDTH=100>
             <img src="images/mail.ico" WIDTH=38 HEIGHT=40>
             <a href="mailto:arasanba@gmail.com">arasanba@gmail.com</a>
-            <img src="images/skype.ico" WIDTH=38 HEIGHT=40>
-            <p>sara_werec</p>
         </TD>
     </TR>
 
@@ -32,8 +26,6 @@
         <TD WIDTH=100>
         <img src="images/mail.ico" WIDTH=38 HEIGHT=40>
         <a href="mailto:alejandro.sanchez@linio.com">alejandro.sanchez@linio.com</a>
-        <img src="images/skype.ico" WIDTH=38 HEIGHT=40>
-        <p>miguelesa1</p>
         </TD>
     </TR>
     <TR>
@@ -45,8 +37,6 @@
         <TD WIDTH=100>
         <img src="images/mail.ico" WIDTH=38 HEIGHT=40>
         <a href="mailto:saul.mtz.v@gmail.com">saul.mtz.v@gmail.com</a>
-        <img src="images/skype.ico" WIDTH=38 HEIGHT=40>
-        <p>mr_freeze360</p>
         </TD>
     </TR>
     <TR>
@@ -58,8 +48,10 @@
         <TD WIDTH=100>
         <img src="images/mail.ico" WIDTH=38 HEIGHT=40>
         <a href="mailto:danielp.1803@gmail.com">danielp.1803@gmail.com</a>
-        <img src="images/skype.ico" WIDTH=38 HEIGHT=40>
-        <p>danielp.1803</p>
         </TD>
     </TR>
 </TABLE>
+<br>
+<hr>
+<p align="center"><img src="images/ginyu_force.png"></p>
+<p align="center"><b>Ginyu Team<b> </p>
